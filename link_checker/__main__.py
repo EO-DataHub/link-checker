a8e53f818e4171bf951f37ac9958bb81901d1b00
hcollingwood <hannah.collingwood@telespazio.com>
2025-05-02 16:14:34 +0100

    EODHP-1297 - Update actions 8

--- link_checker/__main__.py
+++ link_checker/__main__.py
@@ -3,35 +3,23 @@ import subprocess
 import sys
 
 
-def run_npm_command(command):
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-
-    if result.stderr:
-        logging.error(result.stderr)
-
-
-def install():
-    logging.info("Install npm")
-    run_npm_command("npm install")
-    run_npm_command("npm install npx")
-    run_npm_command("npm install whatwg-url")
-    run_npm_command("npm install linkinator")
-
-
 def main(url):
-    # install()
-    result = subprocess.run(f"npx linkinator --recurse --verbosity error {url}", shell=True, capture_output=True, text=True)
-    # result = subprocess.run(f"npx linkinator {url}", shell=True, capture_output=True, text=True)
+    result = subprocess.run(
+        f"npx linkinator --recurse --verbosity error {url}",
+        shell=True,
+        capture_output=True,
+        text=True,
+    )  # nosec
 
     if all_responses := result.stderr:
         warnings = {}
-        for error in all_responses.split('\n'):
-            if error.startswith('['):  # Some of the lines contain description with emojisht
+        for error in all_responses.split("\n"):
+            if error.startswith("["):  # Some of the lines contain description with emojisht
                 code, link = error.split()
                 if not warnings.get(code):
                     warnings[code] = []
                 warnings[code].append(link)
-        errors = warnings.pop('[404]')
+        errors = warnings.pop("[404]")
 
         logging.warning(f"Warnings: {warnings}")
         logging.error(f"Errors: {errors}")
